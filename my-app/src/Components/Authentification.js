b330e3f1c3ddafc13de37b6909385b30cf34a019
Ilyas Hassan <ilyashassan4400@gmail.com>
2021-05-24 22:48:03 -0400

    Update Authentification.js

--- my-app/src/Components/Authentification.js
+++ my-app/src/Components/Authentification.js
@@ -9,7 +9,7 @@ class Authentification extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-          isChecked: false,
+          checked: false,
           email: null,
           password: null
         }
@@ -27,38 +27,39 @@ class Authentification extends React.Component {
     }
     handleOnChange = (email, password) => {
 
-        var additionalText = ""
+        var additionalText = null;
      
         let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     
         if( re.test(email)){
             console.log("Email is correct");
-            additionalText = "Email is correct"
+            additionalText = "Email is correct";
 
         } else if (email == "") {
             console.log("Email Cannot Be Empty");
-            additionalText = "Email Cannot Be Empty"
+            additionalText = "Email Cannot Be Empty";
 
 
         } else if (password == ""){
             console.log("Password Cannot be Empty");
-            additionalText = "Password Cannot be Empty"
+            additionalText = "Password Cannot be Empty";
 
-        } else if (password == null && email == null){
+        } else if (password == "" && email == ""){
             console.log("Email and Password Cannot Be Empty");
-            additionalText = "Email and Password Cannot Be Empty"
+            additionalText = "Email and Password Cannot Be Empty";
 
         } else{
             console.log("Email is incorrect");
-            additionalText = "Email is incorrect"
+            additionalText = "Email is incorrect";
 
         }
         return(
             <div>
-            {additionalText}
+            {this.additionalText}
             </div>
            
         )
+        
     
     }
 
@@ -77,94 +78,104 @@ class Authentification extends React.Component {
 
           <t style={{fontSize: "small"}}>Already a member? <t className="UnderlinedRed">Login instead</t></t>
           <Button className="ContinueButton"
-      h="4rem"
-      w="90%"
-      height="10%"
-      m={{ x: "1.75rem" }}
-      p={{ x: "1.75rem" }}
-      textSize="body"
-      textColor="info700"
-      hoverTextColor="info900"
-      bgc="white"
-      hoverBg="info200"
-      border="1px solid"
-      borderColor="info700"
-      hoverBorderColor="info900"
-    >
-       <Image
-        shadow="3"
-        width="5%"
-        justifyContent='center'
-        border={{ b: "3px solid" }}
-        borderColor="gray400"
-        m={{ r: "2rem" }}
-        bg='white'
-        rounded="xl"
-        src={GoogleLogo}
-        />
-         &nbsp;Continue with google
-    </Button>
-    <label className="OrSignUpText">
-          <input className="OrSignUpText"
-            type="checkbox"
-            checked={this.state.isChecked}
-            onChange={this.handleInputChange} 
-            />
+            h="4rem"
+            w="90%"
+            height="10%"
+            m={{ x: "1.75rem" }}
+            p={{ x: "1.75rem" }}
+            textSize="body"
+            textColor="info700"
+            hoverTextColor="info900"
+            bgc="white"
+            hoverBg="info200"
+            border="1px solid"
+            borderColor="info700"
+            hoverBorderColor="info900"
+            >
+                <Image
+                shadow="3"
+                width="5%"
+                justifyContent='center'
+                border={{ b: "3px solid" }}
+                borderColor="gray400"
+                m={{ r: "2rem" }}
+                bg='white'
+                rounded="xl"
+                src={GoogleLogo}
+                />
+
+                &nbsp;Continue with google
+            </Button>
+
+            <label className="OrSignUpText">
+                <input className="OrSignUpText"
+                type="checkbox"
+                checked={this.state.checked}
+                onChange={this.handleInputChange} 
+                />
+
             or sign up with email
-        </label>
-    <Text>Email</Text>
+            </label>
 
-<form>
-  <label>
-    <input type="email" value={this.state.email} onChange={this.handleOnChange} />
-  </label>
-</form>
+        <Text>Email</Text>
 
-<p>Password (min. 8 characters)</p>
-<form>
-<label>
-    <input type="text" value={this.state.password} onChange={this.handleOnChange} />
-  </label>
+        <form>
+            <label>
+            <input type="email" value={this.state.email} onChange={this.handleOnChange} />
+            </label>
+        </form>
 
-</form>
+        <p>Password (min. 8 characters)</p>
+        <form>
+            <label>
+            <input type="text" value={this.state.password} onChange={this.handleOnChange} />
+            </label>
 
-<form >
-<label>
-          <input
+        </form>
+      
+        <form >
+            <label>
+            <input
             type="checkbox"
-            checked={this.state.isChecked}
+            checked="false"
             onChange={this.handleInputChange} 
             />
+            <input type="checkbox"></input>
+            
             I agree to the <t className="UnderlinedRed" >Terms of Service </t> and <t className="UnderlinedRed">Privacy Policy</t>
-        </label>
-</form>
-<Button className="GetStartBTN"
-      h="4rem"
-      w="90%"
-      width="100%"
-      m={{ x: "1.75rem" }}
-      p={{ x: "1.75rem" }}
-      textSize="body"
-      textColor="white"
-      justifyContent='right'
-      hoverTextColor="info900"
-      bg="white"
-      hoverBg="info200"
-      border="1px solid"
-      borderColor="info700"
-      onClick={this.handleOnChange(this.state.email, this.state.password)}
-      hoverBorderColor="info900"
-    >
-      Get Started
-    </Button>
+            </label>
+
+        </form>
+<       Button className="GetStartBTN"
+        h="4rem"
+        w="90%"
+        width="100%"
+        m={{ x: "1.75rem" }}
+        p={{ x: "1.75rem" }}
+        textSize="body"
+        textColor="white"
+        justifyContent='right'
+        hoverTextColor="info900"
+        bg="white"
+        hoverBg="info200"
+        border="1px solid"
+        borderColor="info700"
+        onClick={this.handleOnChange(this.state.email, this.state.password)}
+        hoverBorderColor="info900"
+        >
+        Get Started
+        </Button>
+
 <br></br>
+
 {this.additionalText}
             
           </Container>
+
         </div>
     );
 
-    }
+}
     
 }
 
